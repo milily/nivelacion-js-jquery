97bc12d77e76a4a40e79ceec4d7dac0ef23e12c8
mily rivas <mrise.translations@gmail.com>
2017-07-10 15:46:13 -0400

    etapa 4 lista, creé html con Jquery con .append() e imprimí las recetas

--- js/script-index.js
+++ js/script-index.js
@@ -30,19 +30,22 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
-	$(".title-recipe").append(` <a class="item-recipe" href="#">
+	$(".list-recipes").append(` <a class="item-recipe" href="#">
                                         <span class="attribution">
-                                            <span class="title-recipe"> TITULO DE LA RECETA (ATRIBUTO "title" ) </span>
-										    <span class="metadata-recipe">
-										      <span class="author-recipe"> NOMBRE DEL AUTO DE LA RECETA (ATRIBUTO "source.name") </span>
+                                            <span class="title-recipe">` + recipe.title + `
+                                            ´ </span>
+										     <span class="metadata-recipe">
+										      <span class="author-recipe"> `+ recipe.source.name + ` </span>
 										      <span class="bookmarks-recipe">
 										        <span class="icon-bookmark"></span> 
 										      </span>
 										    </span>
                                         </span>
+                                        <img src="img/recipes/320x350/`+ recipe.name +`.jpg"/>
                                 </a>`)
 	console.log('Voy a pintar la receta: ', recipe);
 }
+
 
 
 
